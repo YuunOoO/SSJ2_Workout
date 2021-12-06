5fb3cbf307fab190917c0ca8275950097bc894b5
Jaqqas <91982773+Jaqqas@users.noreply.github.com>
2021-12-06 17:02:20 +0100

    Ver. 0.2.1.8  - ikona, scroll menu boczne

--- SSJ2_Workout.Android/Resources/Resource.designer.cs
+++ SSJ2_Workout.Android/Resources/Resource.designer.cs
@@ -11818,19 +11818,22 @@ namespace SSJ2_Workout.Droid
 			public const int p7 = 2131165337;
 			
 			// aapt resource value: 0x7F07009A
-			public const int RB = 2131165338;
+			public const int p8 = 2131165338;
 			
 			// aapt resource value: 0x7F07009B
-			public const int test_custom_background = 2131165339;
+			public const int RB = 2131165339;
 			
 			// aapt resource value: 0x7F07009C
-			public const int tooltip_frame_dark = 2131165340;
+			public const int test_custom_background = 2131165340;
 			
 			// aapt resource value: 0x7F07009D
-			public const int tooltip_frame_light = 2131165341;
+			public const int tooltip_frame_dark = 2131165341;
 			
 			// aapt resource value: 0x7F07009E
-			public const int xamarin_logo = 2131165342;
+			public const int tooltip_frame_light = 2131165342;
+			
+			// aapt resource value: 0x7F07009F
+			public const int xamarin_logo = 2131165343;
 			
 			static Drawable()
 			{
